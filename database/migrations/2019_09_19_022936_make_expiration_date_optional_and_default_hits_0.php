c7d1f869497f602c70f4166c9aeb8a16c4e70f8d
Sushil Gupta <deSushil@gmail.com>
2019-09-19 09:35:04 +0700

    Missing migration from prev. added

---
+++ database/migrations/2019_09_19_022936_make_expiration_date_optional_and_default_hits_0.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeExpirationDateOptionalAndDefaultHits0 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('links')) {
+            Schema::table('links', function (Blueprint $table) {
+                $table->dateTime('expiration_date')->nullable()->change();
+                $table->unsignedBigInteger('hits')->default(0)->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
